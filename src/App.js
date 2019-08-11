9fe2f697f00588c4f9f8938a4695fc5e2a2b267a
YU-HSIN, CHEN <s63232@gmail.com>
2019-08-11 19:44:01 +0800

    feat: set up App entry point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import './styles/styles.scss'
 
-function App() {
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
